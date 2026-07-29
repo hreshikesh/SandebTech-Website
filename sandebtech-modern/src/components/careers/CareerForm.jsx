1d58e18a3d23b3fde98bd965a72a7638bd7dade8
Hreshikesh <ailhreshikesh@gmail.com>
2026-07-29 16:06:39 +0530

    updated google for button

--- sandebtech-modern/src/components/careers/CareerForm.jsx
+++ sandebtech-modern/src/components/careers/CareerForm.jsx
@@ -24,14 +24,15 @@ function CareerForm() {
         </div>
 
         <div className="career-form-wrapper">
-
-          <iframe
-            title="SandebTech Career Form"
-            src="https://docs.google.com/forms/d/e/1FAIpQLSd-MUDv26isMiOezy35qkFSfYsCR6kdeFYo24JvSpNSFonKQw/viewform?usp=sharing&ouid=107933695803050452309"
-            loading="lazy"
-            allowFullScreen
-          />
-
+          <div style="text-align: center; padding: 40px 20px;">
+            <h2>Apply for a Position at SandebTech</h2>
+            <p>Click below to open our application form and upload your documents.</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSd-MUDv26isMiOezy35qkFSfYsCR6kdeFYo24JvSpNSFonKQw/viewform?usp=sharing"
+              target="_blank"
+              style="background-color: #0b57d0; color: white; padding: 12px 24px; text-decoration: none; font-weight: bold; border-radius: 4px; display: inline-block;">
+              Open Application Form
+            </a>
+          </div>
         </div>
 
       </div>
